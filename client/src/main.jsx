583be36a13076bd5305f4bb4937705e65ad2dde8
duuuuufe <philippewilson10@gmail.com>
2026-04-18 13:51:40 -0500

    appointments (#2)

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,16 +8,18 @@ import App from './App.jsx'
 import Register from './pages/Register.jsx'
 import Login from './pages/Login.jsx'
 import Dashboard from './pages/Dashboard.jsx'
+import Appointments from './pages/Appointments.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <Routes>
-          <Route path="/"          element={<App />} />
-          <Route path="/register"  element={<Register />} />
-          <Route path="/login"     element={<Login />} />
-          <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+          <Route path="/"             element={<App />} />
+          <Route path="/register"     element={<Register />} />
+          <Route path="/login"        element={<Login />} />
+          <Route path="/dashboard"    element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+          <Route path="/appointments" element={<ProtectedRoute><Appointments /></ProtectedRoute>} />
         </Routes>
       </AuthProvider>
     </BrowserRouter>
